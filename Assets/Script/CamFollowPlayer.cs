e498c80bd31e2b01528f7889c5fa73f98f57084b
Oguzhan Secgel <oguzhansecgel19@icloud.com>
2023-06-07 21:13:22 +0300

    Added Camera Follow
    Added Camera Follow

--- Assets/Script/CamFollowPlayer.cs
+++ Assets/Script/CamFollowPlayer.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class CamFollowPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform cameraTarget;
+    public float sSpeed = 10f;
+    public Vector3 dist;
+    public Transform lookTarget;
+
+    private void LateUpdate()
+    {
+        Vector3 dPos = cameraTarget.position + dist;
+        Vector3 sPos = Vector3.Lerp(transform.position,dPos,sSpeed*Time.deltaTime);
+        transform.position = sPos;
+        transform.LookAt(lookTarget.position);
+    }
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+     
     void Update()
     {
         
